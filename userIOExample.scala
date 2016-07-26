9a691ad668817f9ba31183fbd0ab8c916622d8ba
Harish T Krishnan <harish.t.krishnan@gmail.com>
2016-07-26 15:05:35 -0700

    Simple example to use import statements and read user input from console

--- userIOExample.scala
+++ userIOExample.scala
@@ -1,5 +1,14 @@
+import scala.io.StdIn.{readLine}
+
 object userIOExample {
 	def main (args: Array[String]) {
-	
+		var num = 0
+		do {
+			print("Guess the secret number: ")
+			num = readLine.toInt	
+		} while (num != 6)
+
+		println("You guessed my secret number: "+num)
+		
 	}
 }
