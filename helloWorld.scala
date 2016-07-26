cc7525824618da715f18b1931bb463f54371efd9
Harish T Krishnan <harish.t.krishnan@gmail.com>
2016-07-26 12:20:40 -0700

    Few basic looping structures

--- helloWorld.scala
+++ helloWorld.scala
@@ -1,19 +1,37 @@
 object helloWorld {
 	def main (args: Array[String]) {
+		//initialize variable (value can be changed)
+
+		//String
 		var str = "Hello World"
+
+		//int
 		var i = 0
 
+		//print statement
+		println(str)
 
 		// while loop example
+		println("while loop example")
 		while(i <= 10) {
-			println (str)
+			println (i)
 			i += 1
 		}
 
 		i = 0
 
+		//do-while example
+		println("do-while loop example")
+		do {
+			println(i)
+			i += 1
+		} while (i <= 10)
+
+		i = 0
+
 		//for loop example
 		// simple for loop iterating from 1 to 10
+		println("Simple for loop example")
 		for (i <- 1  to 10 ) {
 			println (i)
 		}
@@ -21,15 +39,18 @@ object helloWorld {
 		i = 0
 
 		// simple list of String
-		var lst = List("a", "b", "c", "d")
+		// initialize value (value cant change)
+		val lst = List("a", "b", "c", "d")
 
 		//for loop to iterate through list
+		println("For loop iterating through a list method 1")
 		for (i <- 0 until lst.length) {
 			println(lst(i))
 		}
 
 		//another way to iterate through list using for loop
 		//here we use i as temp var to hold list items
+		println("For loop iterating through a list method 2")
 		for (i <- lst) {
 			println("List Item: "+i)
 		}
